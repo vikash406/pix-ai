da9be0a2ce92321e213bdd2354ca2f612fd0452f
vikash406 <vikaskarodiya2006@gmail.com>
2026-05-06 16:27:36 +0530

    update code

--- backend/run.py
+++ backend/run.py
@@ -2,9 +2,11 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    # Render environmental variable 'PORT' use karta hai
-    port = int(os.environ.get("PORT", 8000)) 
-    
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
+
+
     uvicorn.run(
         "backend.main:app",
         host="0.0.0.0",
